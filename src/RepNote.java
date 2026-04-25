86eebcfc6ca51141299c5d568899abb98ce55540
kyawzallthwin <56065932+kywzallthwin@users.noreply.github.com>
2026-04-25 22:29:22 +0700

    update the real version of tester of RepNote outputing previous log history

--- src/RepNote.java
+++ src/RepNote.java
@@ -1,37 +1,58 @@
 public class RepNote {
     public static void main(String[] args) {
-        // Equipment smithMachine = new Equipment("Smith Machine", "Machine");
-        // Equipment rope = new Equipment("Rope Attachment", "Cable");
+        System.out.println("=== Booting RepNote Engine (3-Day PPL)===\n");
 
-        // Weightlifting chestPress = new Weightlifting(16.0, 2, 12, "Chest Press Flat", smithMachine);
-        // Weightlifting lateralraise = new Weightlifting(6.5, 1, 8, "Lateral Raise", rope);
+        //create user profile
+        Lifter myProfile = new Lifter("Kyaw Zall Thwin", 54.0);
+        System.out.println("User Profile Loaded: " + myProfile.getName() + "\n");
 
-        // System.out.println("\n--- Updating Chset Press ---");
-        // chestPress.updateWeight(18.0);
-        // chestPress.updateSets(3);
-        // chestPress.displayExercise();
-
-        System.out.println("\nInitializing RepSync Test Environment...\n");
-
+        //create equipment
         Equipment smithMachine = new Equipment("Smith Machine", "Machine");
-        Equipment rope = new Equipment("Rope Attachment", "Cable");
+        Equipment barbell = new Equipment("Barbell", "Free Weight");
+        Equipment cableRope = new Equipment("Rope Attachment", "Cable");
+        Equipment legPressMachine = new Equipment("Leg Press Machine", "Machine");
+        Equipment cableVBar = new Equipment("V-Bar Attachment", "Cable");
         Equipment treadmill = new Equipment("Treadmill", "Cardio Machine");
+        Equipment tBar = new Equipment("Bar Machine", "Machine");
 
-        Weightlifting chestPress = new Weightlifting(16.0, 2, 12, "Chest Press Flat", smithMachine);
-        Weightlifting lateralraise = new Weightlifting(6.5, 1, 8, "Lateral Raise", rope);
-        Cardio postWorkoutRun = new Cardio(15, 2.5, "Cool-down jog", treadmill);
+        // Day 1; Push Day (Chset, Shoulders, Triceps)
+        WorkoutTracker pushDay = new WorkoutTracker("Push", "April 20, 2026");
 
-        System.out.println("\n--- Updating Chset Press ---");
-        chestPress.updateWeight(18.0);
-        chestPress.updateSets(3);
-        chestPress.displayExercise();
+        Weightlifting inclinePress = new Weightlifting("Incline Chest Press", smithMachine, 15.0, 3, 12);
+        Weightlifting flatPress = new Weightlifting("Flat Chest Press", smithMachine, 16.0, 3, 12);
+        Weightlifting tricepPushdown = new Weightlifting("Tricep Pushdown", cableRope, 23.0, 3, 12);
 
-        WorkoutTracker pushDay = new WorkoutTracker("Push", "April 25, 2026");
+        pushDay.addExercise(inclinePress);
+        pushDay.addExercise(flatPress);
+        pushDay.addExercise(tricepPushdown);
 
-        pushDay.addExercise(chestPress);
-        pushDay.addExercise(lateralraise);
-        pushDay.addExercise(postWorkoutRun);
+        // Day 2: Pull day (Back, Biceps)
+        WorkoutTracker pullDay = new WorkoutTracker("Pull", "April 20, 2026");
 
-        pushDay.displayRoutine();
+        Weightlifting latPulldown = new Weightlifting("Lat Pulldown", cableVBar, 32.0, 3, 12);
+        Weightlifting tBarRow = new Weightlifting("T-Bar Row", tBar, 35.0, 3, 12);
+        Weightlifting facePull = new Weightlifting("Face Pull", cableRope, 23.5, 3, 12);
+    
+        pullDay.addExercise(latPulldown);
+        pullDay.addExercise(tBarRow);
+        pullDay.addExercise(facePull);
+
+        /// Day 3: Leg day
+        WorkoutTracker legDay = new WorkoutTracker("Leg", "April 20, 2026");
+
+        Weightlifting legPress = new Weightlifting("Seated Leg Press", legPressMachine, 66.0, 3, 12);
+        Weightlifting rdls = new Weightlifting("RDLs", barbell, 17.0, 3, 12);
+        Cardio upHillWalk = new Cardio("Up-Hill Walk", treadmill, 10, 1.0);
+    
+        legDay.addExercise(legPress);
+        legDay.addExercise(rdls);
+        legDay.addExercise(upHillWalk);
+
+        // Save
+        myProfile.getHistory().addSession(pushDay);
+        myProfile.getHistory().addSession(pullDay);
+        myProfile.getHistory().addSession(legDay);
+
+        myProfile.getHistory().viewAllSessions();;
     }
 }
